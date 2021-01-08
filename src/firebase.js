b3c80a47a2bd3c187b6f25fbd4185204139ece9c
miwzerking <0865697@students.dadeschools.net>
2021-01-07 19:06:42 -0500

    set up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBEHLTu5-r3ZbGgK3N_xuOtZ2I_cCA132E',
+	authDomain: 'e-commerce-760c8.firebaseapp.com',
+	projectId: 'e-commerce-760c8',
+	storageBucket: 'e-commerce-760c8.appspot.com',
+	messagingSenderId: '829039309948',
+	appId: '1:829039309948:web:5409f4294bb204e73705f9',
+	measurementId: 'G-TPTVJMQF3X'
+};
