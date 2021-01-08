9e4ceec7b65c4f184ecc1f6b35b9e7c053303c71
miwzerking <0865697@students.dadeschools.net>
2021-01-08 14:06:58 -0500

    fisnished first day

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+	const [state, dispatch] = useStateValue();
+	let totalPrice = 0;
+	for (let i = 0; i < state.basket.length; i++) {
+		totalPrice += parseFloat(state.basket[i].price);
+		totalPrice = Math.round(totalPrice * 100) / 100;
+	}
+
 	return (
 		<div className='subtotal'>
 			<CurrencyFormat
 				renderText={value => (
 					<>
 						<p>
-							Subtotal ( 0 items ): <strong>{`0`}</strong>
+							Subtotal ( {state.basket.length} items ): <strong>{`$${totalPrice}`}</strong>
 						</p>
 						<small className='subtotal__gift'>
 							<input type='checkbox' />
